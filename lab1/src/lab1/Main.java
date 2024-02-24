00923a8621d606365137b70c97ba5f4450ace355
Preda-Robert <138782945+Preda-Robert@users.noreply.github.com>
2024-02-24 16:29:55 +0200

    Update Main.java

--- lab1/src/lab1/Main.java
+++ lab1/src/lab1/Main.java
@@ -5,18 +5,23 @@ public class Main {
 	public static void main(String[] args) {
 		LandOwner landOwner = new LandOwner();
 
-        Plot square = new Square(5.0);
-        Plot rectangle = new Rectangle(3.0, 4.0);
+        Plot square = new Square(10.0);
+        Plot rectangle = new Rectangle(24.0, 5.0);
         Plot triangle = new Triangle(6.0, 8.0);
         Plot trapezoid = new Trapezoid(4.0, 7.0, 5.0);
-
+        Plot circle = new Circle(3.0);
+        
         landOwner.addPlot(square);
         landOwner.addPlot(rectangle);
         landOwner.addPlot(triangle);
         landOwner.addPlot(trapezoid);
+        //landOwner.addPlot(circle);
 
         double totalArea = landOwner.calculateTotalArea();
         System.out.println("Total area of all plots: " + totalArea);
+        
+        double totalTax = landOwner.calculateTotalTax();
+        System.out.println("Total tax: " + totalTax + " RON");
 	}
 
 }
